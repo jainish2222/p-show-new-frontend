25b746ea96df40752e56b619146f5d37c3c3a4f3
jainish koladiya <155936377+jainish2222@users.noreply.github.com>
2025-11-05 16:39:48 +0530

    next/srcipt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 import { PopupWidget } from "@/components/PopupWidget";
 import Head from "next/head";
-
+import Script from 'next/script';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
